2482ba83f9e8fa1d8b67236e3d732e77da7378d9
Johnathan Yeo <41785142+nlopez98@users.noreply.github.com>
2026-01-21 13:46:46 +0800

    Update impute-mortality.cpp

--- impute-mortality.cpp
+++ impute-mortality.cpp
@@ -1,116 +1,113 @@
 #include <Rcpp.h>
+#include <random>
 using namespace Rcpp;
 using namespace std;
+
+// Global variable to store if death year was due to breast cancer
+int year_bc_death = 0;
 
 // [[Rcpp::export]]
 int schedule_death(NumericVector individualdata, int age, int year_now, 
                    double basemortality, 
                    NumericMatrix amimort, NumericMatrix strokemort, NumericMatrix ckd5mort,
-                   int index) 
+                   int index, int year_diabetes, int year_ami, int year_stroke, int year_emigrated) 
 {
-  //mortality parameters, hazard ratios
   int year_of_death = 0;
-  int gender, race, agecate, column, duration;
-  double deathprob = runif(1)[0];
-  double nonAMImortratio = 1;//was 0.65
+  year_bc_death = individualdata[207];
+  int gender, race;
+  double deathprob = R::runif(0.0, 1.0);
+  double nonAMImortratio = 1.0;
   NumericVector mortalityrates(10);
   NumericVector HRt2dm = NumericVector::create(1.31, 3.01, 4.27, 1.31);
-  
-  //default mortality rate comes from the mortality cube [ethgen, year_of_birth (minimum = 1870), current year(minimum = 1990)]
-  //mortality rates: 0:default 1:diabetes 2:smoking 3-6:cancers 7:AMI 8:Stroke 9:CKD5 10:UKPDS complications
-  if(age >= 40 && individualdata[4] == 0)
-  {
-    mortalityrates[0] = nonAMImortratio * basemortality;
+
+  // -------------------------------
+  // Year factor
+  // -------------------------------
+  double year_factor;
+  if (year_now >= 2013) {
+    year_factor = 1.0;
+  } else if (year_now >= 2003 && year_now <= 2012) {
+    year_factor = (1.0 - 0.797) / (1.0 - 0.817);
+  } else {
+    year_factor = (1.0 - 0.755) / (1.0 - 0.817);
   }
-  else
-  {
+
+  // -------------------------------
+  // Prop factor (5-year buckets)
+  // -------------------------------
+  double prop_factor;
+  if (year_now <= 1972) {
+    prop_factor = 1.0 - 0.774395;
+  } else if (year_now <= 1977) {
+    prop_factor = 1.0 - 0.758405;
+  } else if (year_now <= 1982) {
+    prop_factor = 1.0 - 0.704844;
+  } else if (year_now <= 1987) {
+    prop_factor = 1.0 - 0.691202;
+  } else if (year_now <= 1992) {
+    prop_factor = 1.0 - 0.611708;
+  } else {
+    prop_factor = 1.0 - 0.611708; // copy last factor for >2017
+  }
+
+  // --- Base mortality
+  if(age >= 40 && individualdata[4] == 0) {
+    mortalityrates[0] = nonAMImortratio * basemortality;
+  } else {
     mortalityrates[0] = basemortality;
   }
   
-  //diabetes
-  if(individualdata[4] > 0)
-  {
+  // --- Diabetes mortality
+  if(individualdata[4] > 0) {
     race = int(index/2);
-    mortalityrates[1] = 1- pow(1-basemortality, HRt2dm[race]);
+    mortalityrates[1] = 1 - pow(1 - basemortality, HRt2dm[race]);
   }
   
-  //brca https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8075208/
-  if(individualdata[199] > 0){
-    if(individualdata[202] > 0){
-      if(individualdata[203] > 0){
-        if(individualdata[204] > 0){
-          if(individualdata[205] > 0){
-            mortalityrates[2] = 0.268; //stage 4
+  // --- Breast cancer mortality (stages)
+  if(individualdata[199] > 0) {
+    if(individualdata[202] > 0 && individualdata[202] <= year_now){
+      if(individualdata[203] > 0 && individualdata[203] <= year_now){
+        if(individualdata[204] > 0 && individualdata[204] <= year_now){
+          if(individualdata[205] > 0 && individualdata[205] <= year_now){
+            mortalityrates[2] = 1 - exp(log(0.29) / 5.0);   // stage 4
+          //  mortalityrates[0] = mortalityrates[2] * prop_factor * year_factor;
           } else {
-            mortalityrates[2] = 0.083; //stage 3
+            mortalityrates[2] = 1 - exp(log(0.7) / 5.0);    // stage 3
+           // mortalityrates[0] = mortalityrates[2] * prop_factor * year_factor;
           }
         } else {
-          mortalityrates[2] = 0.044; //stage 2
-      } 
+          mortalityrates[2] = 1 - exp(log(0.88) / 5.0);     // stage 2
+         // mortalityrates[0] = mortalityrates[2] * prop_factor * year_factor;
+        }
       } else {
-        mortalityrates[2] = 0.02; //stage 1
+        mortalityrates[2] = 1 - exp(log(0.96) / 5.0);       // stage 1
+      //  mortalityrates[0] = mortalityrates[2] * prop_factor * year_factor;
       }
     } else {
-      mortalityrates[2] = 0.01; //stage 0
+      mortalityrates[2] = 0;  // stage 0
     }
-      
-     
+  } else {
+    mortalityrates[2] = 0.0;  // no BC mortality
   }
   
-  
-  //UKPDS 8-9:AMI 10-11:Stroke 12:CKD5 13:Ulcers 14:IHD 15:CHF 16:Blindness 17-18:Amputations
-  //AMI
-  /*
-   if(individualdata[8] <= year_now && individualdata[8] > 0 && individualdata[3] == 0)
-   {
-   gender = abs(remainder(index,2));
-   agecate = 0;
-   if(age > 59) agecate = 1;
-   if(age > 69) agecate = 2;
-   if(age > 79) agecate = 3;
-   column = 2 * agecate + gender;
-   duration =  year_now - individualdata[8];
-   if(duration > 10) duration = 10;
-   mortalityrates[7] = amimort(duration,column);
-   // //non-diabetics
-   // if(individualdata[3] == 0)
-   // {
-   //     mortalityrates[7] = amimort(duration,column);
-   // }
-   // //diabetics
-   // else
-   // {
-   //     mortalityrates[7] = amimort(duration,column+8);
-   // }
-   }
-   
-   //Stroke
-   if(individualdata[10] <= year_now && individualdata[10] > 0 && individualdata[3] == 0)
-   {
-   gender = abs(remainder(index,2));
-   agecate = 0;
-   if(age > 65) agecate = 1;
-   column = 2 * gender + agecate;
-   duration =  year_now - individualdata[10];
-   if(duration > 20) duration = 20;
-   mortalityrates[8] = strokemort(duration,column);
-   }
-   
-   //ckd5 This seems to be using ckd5 and esrd interchangeably
-   if(individualdata[12] >= year_now && individualdata[12] > 0 && individualdata[3] == 0)
-   {
-   duration =  year_now - individualdata[12];
-   if(duration > 14) duration = 13;
-   mortalityrates[9] = ckd5mort(duration,2);
-   }
-   
-   */
-  
-  //evaluate if the individual died this year
-  if(deathprob < max(mortalityrates))
-  {
+  // --- Determine year_of_death (only from mortalityrates[0:1]) ---
+  double max_mort_01 = std::max(mortalityrates[0], mortalityrates[1]);
+  if (deathprob < max_mort_01) {
     year_of_death = year_now;
+  }
+  if (year_bc_death==0){
+    
+    // --- Determine year_bc_death (from mortalityrates[0:2]) ---
+    double max_mort_012 = std::max({mortalityrates[0], mortalityrates[1], mortalityrates[2]});
+    if (deathprob < max_mort_012) {
+      year_bc_death = year_now;
+    }
   }
   
   return year_of_death;
+}
+
+// [[Rcpp::export]]
+int get_year_bc_death() {
+  return year_bc_death;
 }
